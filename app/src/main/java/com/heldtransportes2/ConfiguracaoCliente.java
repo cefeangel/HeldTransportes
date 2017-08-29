e28f25f9cc864dac531facb36c3a57fdd6cd7a7e
cefeangel <cefeangel@hotmail.com>
2017-08-29 10:58:02 -0300

    Initial commit

---
+++ app/src/main/java/com/heldtransportes2/ConfiguracaoCliente.java
@@ -0,0 +1,11 @@
+package com.heldtransportes2;
+
+/**
+ * Created by DEV on 10/08/2016.
+ */
+public class ConfiguracaoCliente {
+
+    public static String nomeCliente = "heldtransportes";
+    public static String versaoApp = "2.5";
+
+}
